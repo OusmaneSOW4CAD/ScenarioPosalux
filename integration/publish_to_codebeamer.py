6f15997670440d8271382fce8bce92144f63c572
POC-Posalux <osow@4cad.fr>
2026-09-04 10:52:39 +0200

    Update API endpoint and improve print statements

--- integration/publish_to_codebeamer.py
+++ integration/publish_to_codebeamer.py
@@ -3,12 +3,13 @@ import requests
 USER = "user_posalux"
 PASSWORD = "5nZ0i6SzyZgVbutlonger"
 
-url = "https://pdm13-1-codebeamer.connexateurs.com/cb/v3"
+
+url = "https://pdm13-1-codebeamer.connexateurs.com/cb/rest"
 
 r = requests.get(
     url,
     auth=(USER, PASSWORD)
 )
 
-print(r.status_code)
-print(r.text[:1000])
+print("STATUS =", r.status_code)
+print(r.text)

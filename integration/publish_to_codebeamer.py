983b584f1e2ae78ac336e1411e430aac1c0331be
POC-Posalux <osow@4cad.fr>
2026-09-04 16:17:30 +0200

    Update TEST_CASE_ID to include multiple IDs

--- integration/publish_to_codebeamer.py
+++ integration/publish_to_codebeamer.py
@@ -9,7 +9,7 @@ import requests
 BASE_URL = "https://pdm13-1-codebeamer.connexateurs.com/cb/api/v3"
 
 TEST_SET_RUN_ID = 33898
-TEST_CASE_ID = 1218
+TEST_CASE_ID = 1218, 1216
 
 USER = os.environ["CODEBEAMER_USER"]
 PASSWORD = os.environ["CODEBEAMER_PASSWORD"]

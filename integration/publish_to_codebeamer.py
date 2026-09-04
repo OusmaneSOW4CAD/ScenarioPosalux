e783247cecb6460b62846f533c82418c7faa2d43
POC-Posalux <osow@4cad.fr>
2026-09-04 09:59:42 +0200

    Refactor publish_to_codebeamer.py for new data structure

--- integration/publish_to_codebeamer.py
+++ integration/publish_to_codebeamer.py
@@ -1,12 +1,41 @@
 import json
+import requests
 
-with open("results.json") as f:
+CODEBEAMER_URL = "https://pdm13-1-codebeamer.connexateurs.com/"
+USER = "user_posalux"
+PASSWORD = "5nZ0i6SzyZgVbutlonger"
+
+TEST_RUN_ID = 5001
+TEST_CASE_RUN_ID = 5002
+
+with open("result.json") as f:
     data = json.load(f)
 
 status = (
     "PASSED"
-    if data["max_stress_mpa"] <= data["stress_limit_mpa"]
+    if data["max_stress"] <= data["limit"]
     else "FAILED"
 )
 
-print(f"Test {data['test_case_run_id']} : {status}")
+payload = {
+    str(TEST_CASE_RUN_ID): {
+        "success": status,
+        "conclusion":
+            f"Stress={data['max_stress']} MPa "
+            f"Limit={data['limit']} MPa"
+    }
+}
+
+url = (
+    f"{CODEBEAMER_URL}"
+    f"/rest/testmanagement/testrun/"
+    f"{TEST_RUN_ID}/result"
+)
+
+response = requests.put(
+    url,
+    json=payload,
+    auth=(USER, PASSWORD)
+)
+
+print(response.status_code)

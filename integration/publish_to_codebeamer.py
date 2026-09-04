a543345b23f147582b9fad44167226d47d9896a6
POC-Posalux <osow@4cad.fr>
2026-09-04 10:43:51 +0200

    Simplify API request to a single project URL
    Refactor code to use a single URL for requests with authentication.

--- integration/publish_to_codebeamer.py
+++ integration/publish_to_codebeamer.py
@@ -1,17 +1,15 @@
 import requests
 
-urls = [
-    "https://pdm13-1-codebeamer.connexateurs.com/cb/rest",
-    "https://pdm13-1-codebeamer.connexateurs.com/cb/rest/user",
-    "https://pdm13-1-codebeamer.connexateurs.com/cb/rest/project",
-    "https://pdm13-1-codebeamer.connexateurs.com/cb/rest/configuration"
-]
+USER = "user_posalux"
+PASSWORD = "5nZ0i6SzyZgVbutlonger"
 
-for url in urls:
-    try:
-        r = requests.get(url, timeout=10)
-        print(url)
-        print(r.status_code)
-        print("-" * 50)
-    except Exception as e:
-        print(url, e)
+url = "https://pdm13-1-codebeamer.connexateurs.com/cb/rest/project"
+
+r = requests.get(
+    url,
+    auth=(USER, PASSWORD)
+)
+
+print(r.status_code)
+print(r.text[:1000])
+`
